2275dcc50d7361b79441d7820e1fa1fa5a5ba216
Pulimet <korolev2009@gmail.com>
2024-10-25 02:24:14 +0300

    Clean

--- app/src/main/java/net/alexandroid/getrandomtask/ui/theme/Theme.kt
+++ app/src/main/java/net/alexandroid/getrandomtask/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package net.alexandroid.getrandomtask.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
