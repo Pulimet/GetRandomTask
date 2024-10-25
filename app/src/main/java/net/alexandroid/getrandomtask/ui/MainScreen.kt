6f88e31791f504b68b38fa5183b2af806fd7b87b
Pulimet <korolev2009@gmail.com>
2024-10-25 21:30:20 +0300

    MainScreen.kt - Make content scroll + Refactoring

--- app/src/main/java/net/alexandroid/getrandomtask/ui/MainScreen.kt
+++ app/src/main/java/net/alexandroid/getrandomtask/ui/MainScreen.kt
@@ -1,6 +1,9 @@
 package net.alexandroid.getrandomtask.ui
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,43 +13,56 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import net.alexandroid.getrandomtask.R
+import net.alexandroid.getrandomtask.model.Task
 import net.alexandroid.getrandomtask.viewmodel.TaskViewModel
 import org.koin.androidx.compose.koinViewModel
 
 
 @Composable
-fun MainScreen(
-    modifier: Modifier = Modifier,
-    viewModel: TaskViewModel = koinViewModel()
-) {
+fun MainScreen(modifier: Modifier = Modifier) {
+    Content(modifier)
+    ShowTaskDialog()
+    AddTaskDialog()
+}
+
+@Composable
+private fun Content(modifier: Modifier = Modifier, viewModel: TaskViewModel = koinViewModel()) {
     val tasks by viewModel.getAllTasks().collectAsState(initial = emptyList())
 
-    Column(modifier = modifier) {
-        LazyColumn {
-            items(tasks) { task ->
-                TaskItem(task)
-            }
-        }
-        Button(
-            onClick = {
-                viewModel.showShowTaskDialog(tasks)
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Text(stringResource(R.string.wanna_task))
+    Box(modifier = modifier.fillMaxSize()) {
+        TasksList(tasks)
+        WannaTaskButton(viewModel, tasks)
+    }
+}
+
+@Composable
+private fun TasksList(tasks: List<Task>) {
+    LazyColumn(modifier = Modifier.padding(bottom = 60.dp)) {
+        items(tasks) { task ->
+            TaskItem(task)
         }
     }
+}
 
-    ShowTaskDialog()
-
-    AddTaskDialog()
+@Composable
+private fun BoxScope.WannaTaskButton(viewModel: TaskViewModel, tasks: List<Task>) {
+    Button(
+        onClick = {
+            viewModel.showShowTaskDialog(tasks)
+        },
+        modifier = Modifier
+            .align(Alignment.BottomCenter)
+            .padding(vertical = 16.dp, horizontal = 80.dp)
+            .fillMaxWidth()
+    ) {
+        Text(stringResource(R.string.wanna_task))
+    }
 }
 
 
